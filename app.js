786680ccf4424caf73f9585a3f75d1ffe4bc993c
Lance <www.lancecruz@gmail.com>
2024-07-07 20:04:35 +0800

    pug

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
