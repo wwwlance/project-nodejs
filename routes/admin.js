6bf740bdd5cc0826bf2617a547ba170771c808e6
Lance <www.lancecruz@gmail.com>
2024-07-07 16:17:34 +0800

    files

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/shop', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
